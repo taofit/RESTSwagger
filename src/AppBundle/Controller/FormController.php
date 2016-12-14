8804399288279bb17c9f063d6b76bc29606249f4
taofit <tao_sun@hotmail.com>
2016-12-14 15:25:42 +0100

    yes

--- src/AppBundle/Controller/FormController.php
+++ src/AppBundle/Controller/FormController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -23,10 +24,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  */
 class FormController extends Controller {
     /**
-     *@Route("/onefiorm", name="have fun")
+     * @Route("/onefiorm", name="have fun")
      */
     public function newAction() {
-        return new Response("I can change it:" );
+        return new Response("I can change it now johnny" );
 //        $task = new Task();
 //        $task->setTask("Write a blog post");
 //        $task->setDueDate(new \DateTime('tomorrow'));
